196023a7e0a2050ebf6373fe9733440a45495046
Adam Chow <adamchow2326@yahoo.com.au>
2024-05-05 17:49:48 +1000

    reuse password validation rule

--- src/validationSchemas/signUp.ts
+++ src/validationSchemas/signUp.ts
@@ -4,27 +4,27 @@ import {
     EMAIL_ERROR 
 } from 'constants/index';
 
-
+const passwordValidationRule = yup.string()
+    .min(3, 'Must at least 3 characters')
+    .required(REQUIRED_FIELD)
+    .matches(
+        /^(?=.*[A-Z])/,
+        '  Must contain at least one uppercase'
+    )
+    .matches(
+        /^(?=.*[0-9])/,
+        '  Must contain at least one number'
+    )
+    .matches(
+        /^(?=.*[\W])/,
+        '  Must contain at least one special character'
+    );
   
 // sign up form schema
 export const signUpFormSchema = yup.object({
     email: yup.string().email().required(EMAIL_ERROR),
-    password: yup.string()
-        .min(3, 'Must at least 3 characters')
-        .required(REQUIRED_FIELD)
-        .matches(
-            /^(?=.*[A-Z])/,
-            '  Must contain at least one uppercase'
-        )
-        .matches(
-            /^(?=.*[0-9])/,
-            '  Must contain at least one number'
-        )
-        .matches(
-            /^(?=.*[\W])/,
-            '  Must contain at least one special character'
-        ),
-    confirmPassword: yup.string().test('passwords-match', 'Passwords must match', function passwordsMatch(value){
+    password: passwordValidationRule,
+    confirmPassword: passwordValidationRule.test('passwords-match', 'Passwords must match', function passwordsMatch(value){
         return this.parent.password === value;
     }),
 });
